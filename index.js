cdbf5b73e177e58cbdac583eed50ae4106b58377
Ricardo Pereira <m@ricardopereira.eu>
2016-07-21 18:49:40 +0100

    Add dependency: multer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var data = multer(); // for parsing multipart/form-data
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -20,7 +22,7 @@ app.get('/', function(request, response) {
 
 app.post('/slack/message_action/', function(request, response) {
 	console.log('Request:', request.body)
-	console.log('Content-Type:', request.get('Content-Type'))
+	console.log('Data:', request.body['payload']['actions'])
 	response.sendStatus(200)
 });
 
