390a6f4c5616aa19db6a4623c77507a1441a76d0
Ricardo Pereira <m@ricardopereira.eu>
2016-07-21 17:21:06 +0100

    Body parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
