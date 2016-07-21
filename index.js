3b6d9694be3eea4e94b064d48d63c189c1f7a10c
Ricardo Pereira <m@ricardopereira.eu>
2016-07-21 18:18:06 +0100

    Add urlencoded body parser

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw());
 
 app.use(express.static(__dirname + '/public'));
